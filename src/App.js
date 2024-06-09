9f558d545e765ae620ede76ee942dd4333695f75
lasindu <lasindudulanjana44@gmail.com>
2024-06-09 23:29:46 +0530

    About Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import React from "react";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
+import About from "./pages/About";
 
 import './App.css';
 
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" exact Component={Home}/>
         <Route path="/menu" exact Component={Menu}/>
-
+        <Route path="/about" exact Component={About}/>
       </Routes >
       <Footer/>
       </Router>
